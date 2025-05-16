7b50f45e8b6a4030b87db74068911887f34b6a39
Glenn Greibesland <ggr@effera.no>
2025-05-16 14:34:37 +0200

    fix New Partition command and only reassign DVD drive if an uninitialized disk exists

--- initialize-vm-temporary-storage.ps1
+++ initialize-vm-temporary-storage.ps1
@@ -1,26 +1,29 @@
-# Reassign D: to E: if it exists
-$dvdDrive = Get-WmiObject Win32_Volume | Where-Object { $_.DriveType -eq 5 }
-if ($dvdDrive) {
-    $dvdDrive.DriveLetter = $null
-    $dvdDrive.Put()
-
-    Start-Sleep -Seconds 5
-
-    $dvdDrive.DriveLetter = 'E:'
-    $dvdDrive.Put()
-    Start-Sleep -Seconds 5
-}
-
+# This script initializes the temporary storage disk in a Windows VM.
+# It reassigns the DVD drive letter to E: if a an uninitialized disk exists,
+# and initializes the temporary disk, formats and assigns it to drive letter D.
 
 # Find uninitialized disk (likely temporary disk)
 $disk = Get-Disk | Where-Object { $_.PartitionStyle -eq 'RAW' -and $_.OperationalStatus -eq 'Online' }
 
 if ($disk) {
+    # Reassign dvd drive D: to E: if it exists
+    $dvdDrive = Get-WmiObject Win32_Volume | Where-Object { $_.DriveType -eq 5 }
+    if ($dvdDrive) {
+        $dvdDrive.DriveLetter = $null
+        $dvdDrive.Put()
+
+        Start-Sleep -Seconds 5
+
+        $dvdDrive.DriveLetter = 'E:'
+        $dvdDrive.Put()
+        Start-Sleep -Seconds 5
+    }
+
     # Initialize with GPT
     Initialize-Disk -Number $disk.Number -PartitionStyle GPT -Confirm:$false
 
     # Create a new partition using all space
-    $partition = New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter -DriveLetter 'D'
+    $partition = New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter
 
     # Format it as NTFS and label it
     Format-Volume -Partition $partition -FileSystem NTFS -NewFileSystemLabel "Temporary Storage" -Confirm:$false
