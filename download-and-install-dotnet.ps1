0348cc63809517bd3d13b616bbbeb204dc477f1d
Glenn Greibesland <ggr@effera.no>
2025-05-26 14:12:17 +0200

    install aspnetcore as well

--- download-and-install-dotnet.ps1
+++ download-and-install-dotnet.ps1
@@ -1,4 +1,6 @@
 # The install script should be downloaded by the CustomScriptExtension on the VM ScaleSet
 .\dotnet-install.ps1 -Runtime dotnet -Channel 9.0 -Quality GA -InstallDir "C:\Program Files\dotnet"
+.\dotnet-install.ps1 -Runtime aspnetcore -Channel 9.0 -Quality GA -InstallDir "C:\Program Files\dotnet"
 
+# Ensure the dotnet installation directory is in the system PATH
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\dotnet", [System.EnvironmentVariableTarget]::Machine)
