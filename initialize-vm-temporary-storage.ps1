126f5139d81637081004dc687901e662ea51117a
Glenn Greibesland <ggr@effera.no>
2025-07-17 12:46:34 +0200

    fix support for initialization of several disks and labeling

--- initialize-vm-temporary-storage.ps1
+++ initialize-vm-temporary-storage.ps1
@@ -1,30 +1,31 @@
-# This script initializes the temporary storage disk in a Windows VM.
-# It reassigns the DVD drive letter to Z: if a an uninitialized disk exists,
-# and initializes the temporary disk, formats and assigns it to drive letter D and continues to assign subsequent disks to E, F, etc.
+# Reassign DVD to Z: if needed
+$dvdDrive = Get-WmiObject Win32_Volume | Where-Object { $_.DriveType -eq 5 }
+if ($dvdDrive) {
+    $dvdDrive.DriveLetter = $null
+    $dvdDrive.Put()
+    Start-Sleep -Seconds 5
+    $dvdDrive.DriveLetter = 'Z:'
+    $dvdDrive.Put()
+    Start-Sleep -Seconds 5
+}
 
-# Find uninitialized disk (likely temporary disk)
-$disk = Get-Disk | Where-Object { $_.PartitionStyle -eq 'RAW' -and $_.OperationalStatus -eq 'Online' }
+# Identify all RAW, online disks
+$rawDisks = Get-Disk | Where-Object { $_.PartitionStyle -eq 'RAW' -and $_.OperationalStatus -eq 'Online' }
 
-if ($disk) {
-    # Reassign dvd drive D: to Z: if it exists
-    $dvdDrive = Get-WmiObject Win32_Volume | Where-Object { $_.DriveType -eq 5 }
-    if ($dvdDrive) {
-        $dvdDrive.DriveLetter = $null
-        $dvdDrive.Put()
+foreach ($disk in $rawDisks) {
+    $label = "Storage Disk"  # Default
 
-        Start-Sleep -Seconds 5
-
-        $dvdDrive.DriveLetter = 'Z:'
-        $dvdDrive.Put()
-        Start-Sleep -Seconds 5
+    # Heuristics to identify ephemeral/tempo disk
+    if (
+        $disk.Model -match "Direct Disk"
+    ) {
+        $label = "Temporary Storage"
     }
 
-    # Initialize with GPT
+    Write-Host "Initializing Disk #$($disk.Number) as '$label'..."
+
     Initialize-Disk -Number $disk.Number -PartitionStyle GPT -Confirm:$false
-
-    # Create a new partition using all space
     $partition = New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter
-
-    # Format it as NTFS and label it
-    Format-Volume -Partition $partition -FileSystem NTFS -NewFileSystemLabel "Temporary Storage" -Confirm:$false
+    Start-Sleep -Seconds 5
+    Format-Volume -Partition $partition -FileSystem NTFS -NewFileSystemLabel $label -Confirm:$false
 }
